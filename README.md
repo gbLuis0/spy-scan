e6353aa3a50797707d6e3db0fefbea4b69ae6e1f
Spyware <gabrielluisdm13@gmail.com>
2022-03-26 14:55:09 -0300

    Up main and readme

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ![](https://camo.githubusercontent.com/71b837571c48af3aa60a73dbc9d5936aa359d78efbfa8a6743cbbbc16b80ef4d/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f3830353930323039333930363630383138362f3830353931333937323533353539303932322f74656e6f722e676966)
+
 # ***Spy Scan***
 
 Um simples port scanner feito em python
@@ -13,12 +14,12 @@ apt-get upgrade -y
 apt-get install git python -y
 ```
 
-## Acesso 📂
+## Ajuda do script 📂
 ```bash
 cd spy-scan
 python main.py
 ```
 
-Se quiser elogiar, criticar e etc, me chame no [Whatsapp](http://wa.me/559885267746)!
+Se quiser elogiar, criticar e etc, me chame no [Whatsapp](http://wa.me/559885267746) ou deixe uma [Issue](https://github.com/Spyware0/spy-scan/issues/new)!
 
 ![](https://camo.githubusercontent.com/71b837571c48af3aa60a73dbc9d5936aa359d78efbfa8a6743cbbbc16b80ef4d/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f3830353930323039333930363630383138362f3830353931333937323533353539303932322f74656e6f722e676966)
--- main.py
+++ main.py
@@ -1,32 +1,56 @@
-from socket import socket, AF_INET, SOCK_STREAM
 from os import system, name
-try:
-    from pyfiglet import Figlet
-except:
-    system('pip install pyfiglet -y')
-finally:
-    from pyfiglet import Figlet
-
-banner = Figlet(font='slant').renderText('Spy-scan')
-
-def cls():
-    system("cls" if name == 'nt' else "clear")
+import argparse
+from sys import argv
+from socket import socket, AF_INET, SOCK_STREAM
 
 portas = [20, 21, 22, 23, 25, 53, 80, 118, 119, 135, 143, 443, 3306, 8080]
+time = 0.4
+n = '\033[1m'
 az = '\033[34;1m'
 f = '\033[m'
-n = '\033[1m'
-per = 'y'
 
-while per == 'y':
-    cls()
-    print(banner)
-    host = input(n+'Digite IP ou domínio: ').strip().lower()
-    print('Port ', ' Situation\n-----------------')
+parser = argparse.ArgumentParser(
+    description="Um Port Scanner; by: spyware",
+    usage=f'{argv[0]} -d HOST')
+
+parser.add_argument(
+    '-d', '--host',
+    help="Host que será escaneado"
+)
+
+parser.add_argument(
+    '-n', '--nivelscan', type=int, metavar='1 or 2',
+    help='Portas para escanear; 1: principais portas; 2: todas as portas (65.536) (opcional)'
+)
+
+parser.add_argument(
+    '-t', '--time', type=float,
+    help='Tempo para escaneamento (segundos) (opcional)'
+)
+
+opt = {1: portas, 2: list(range(65536))}
+
+args = parser.parse_args()
+if args.nivelscan:
+    portas = opt[args.nivelscan]
+
+if args.time:
+    time = args.time
+
+system('cls' if name == 'nt' else 'clear')
+if len(argv) <= 1:
+    parser.print_help()
+    exit(0)
+
+if args.host:
+    print(f'Scanning {n}{args.host}{f}...')
+    print('Port  ', '  Situation\n-------------------')
     for p in portas:
         con = socket(AF_INET, SOCK_STREAM)
-        con.settimeout(0.45)
-        rsp = con.connect_ex((host, p))
-        print(f'{az}{p:<7} \033[32;1mOPEN{f}' if rsp == 0 else f'{az}{p:<7} \033[31;1mCLOSED{f}')
-    per = input(f'{n}Deseja realizar outro scan? [{az}y{f}/{az}n{f}] ').strip().lower()[0]
-cls()
+        con.settimeout(time)
+        rsp = con.connect_ex((args.host, p))
+        con.close()
+        if rsp == 0:
+            print(f'{az}{p:<9} \033[32;1mOPEN{f}'+' '*50)
+        else:
+            print(f'{az}{p:<9} \033[31;1mCLOSED{f}'+' '*50, end='\r')
